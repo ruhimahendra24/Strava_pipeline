54fa6d1cdc48b01a382c2b592ec48820c49cfc17
ruhimahendra24 <ruhimahendra24@gmail.com>
2023-03-02 14:01:38 -0500

    new

--- API_EXTRACT/strava_API_connection.py
+++ API_EXTRACT/strava_API_connection.py
@@ -3,19 +3,17 @@ import requests
 import json
 
 # CREDENTIALS
-client_id = 79117
-client_secret = '96a73b79661036fa04c67697846e22d40789b21a'
-redirect_url = 'http://localhost/'
-code = '41d9decced7edc397c32376e53c24e72ef13787e'
-  
+key_file = open('/Users/ruhimahendra/Desktop/Strava_pipeline/API_EXTRACT/access.txt')
+lines = key_file.readlines()
+
 # REQUEST URL RESPONSE
 response = requests.post(
                     url = 'https://www.strava.com/oauth/token',
                     data = {
-                            'client_id': client_id,
-                            'client_secret': client_secret,
-                            'code': code,
-                            'grant_type': 'authorization_code'
+                            'client_id': lines[0].split(':')[1].rstrip(),
+                            'client_secret': lines[1].split(':')[1].rstrip(),
+                            'code': lines[2].split(':')[1].rstrip(),
+                            'grant_type': lines[3].split(':')[1].rstrip()
                             }
                 )
 
