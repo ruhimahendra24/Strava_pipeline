4b8db4b4771a2c7eb6a4d1c586c7286da391abc5
ruhimahendra24 <ruhimahendra24@gmail.com>
2023-03-13 19:53:52 -0400

    added activities to database

--- AIRFLOW/strava_pipeline.py
+++ AIRFLOW/strava_pipeline.py
@@ -8,6 +8,7 @@ import time
 import pendulum
 from airflow.decorators import dag, task
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 
 #%%
 @dag(dag_id = 'strava_activities',
@@ -99,8 +100,27 @@ def strava_summary():
                     activities.loc[x + (page-1)*200,'external_id'] = r[x]['external_id']
             # increment page
                 page += 1
-            return activities.to_json()
+            return activities.to_json(orient='records')
         strava_activies = get_activities()
                     
         create_database.set_downstream(strava_activies)
+    
+    
+        @task()
+        def load_activities(activties):
+            hook = SqliteHook(sqlite_conn_id="activities")
+            stored_activties = hook.get_pandas_df("SELECT * from runs;")
+            new_activties = []
+            activties = json.loads(activties)
+            for activity in activties:
+                print(activity)
+                if activity["id"] not in stored_activties["id"].values:
+                    new_activties.append([activity['id'],activity['name'],activity['start_date_local'],activity['type'],activity['distance'],activity['moving_time'],activity['elapsed_time'],activity['total_elevation_gain'],activity['end_latlng'],activity['external_id']])
+
+            hook.insert_rows(table='runs', rows=new_activties, target_fields=["id", "name", "start_date_local", "type", "distance", "moving_time", "elapsed_time", "total_elevation_gain", "end_latlng", "external_id"])
+            return new_activties
+
+        new_activties = load_activities(strava_activies)
+        
+    
 summary = strava_summary()
